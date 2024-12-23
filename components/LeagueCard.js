01b2eaad5f1db3f2f772f5becdab3dd1df344bc6
Sid Dayaneni <79270579+sdayaneni@users.noreply.github.com>
2024-12-23 02:56:58 -0600

    Full upload

--- components/LeagueCard.js
+++ components/LeagueCard.js
@@ -1,16 +1,23 @@
-import React from 'react';
-import {View, Text, ImageBackground, StyleSheet, Pressable, Image, TouchableOpacity} from 'react-native';
+import React, { useState, useEffect } from "react";
+import {View, Text, ImageBackground, StyleSheet, Pressable, Image, TouchableOpacity, TouchableHighlight} from 'react-native';
 import { useNavigation } from '@react-navigation/core'
 import placeholder from "../assets/placeholder.jpeg"
 import {getFirestore, collection, addDoc, doc, setDoc, getDoc, getDocs, deleteDoc, query, orderBy} from 'firebase/firestore';
-import {db} from "../firebase.js"
+import {db} from "../firebase.js";
+import { Swipeable } from 'react-native-gesture-handler';
+import {themeColor, useTheme } from "react-native-rapi-ui";
+import { Ionicons } from '@expo/vector-icons';
+
+
 
 // import theme from '../../assets/themes';
 // import Avatars from './Avatars';
 
-const LeagueCard = ({item, title, size, numJoined, width, newMargin, user, leagueId}) => {
+const LeagueCard = ({item, title, size, numJoined, width, newMargin, user, leagueId, onDelete}) => {
 
     const navigation = useNavigation()
+    const [isPressed, setIsPressed] = useState(false); // State to track button press
+
 
     async function handleClick(){
         if(leagueId == undefined) {
@@ -46,29 +53,77 @@ const LeagueCard = ({item, title, size, numJoined, width, newMargin, user, leagu
             }
         }
     }
+
+    const renderRightActions = (progress, dragX) => {
+        return (
+          <TouchableHighlight
+            onPress={() => {
+                console.log("deleting")
+                if (onDelete) {
+                    onDelete(leagueId);
+                  }
+            //   if (props.onDelete) {
+            //     props.onDelete(props.eventId);
+            //   }
+              setIsPressed(true); // Set button state to pressed
+              setTimeout(() => setIsPressed(false), 200); // Reset button state after 200ms
+            }}
+            style={{
+              backgroundColor: isPressed ? themeColor.danger600 : themeColor.danger,
+              justifyContent: 'center',
+              alignItems: 'flex-end',
+              width: 100,
+            //   marginRight: 20,
+            //   marginLeft: -30,
+            //   marginVertical: 10,
+            marginBottom: 20,
+              borderTopRightRadius: 10,
+              borderBottomRightRadius: 10,
+              paddingRight: 35,
+            }}
+            underlayColor={themeColor.danger600} // Color when button is pressed
+          >
+            <View
+              style={{
+                justifyContent: 'center',
+                alignItems: 'center',
+                flex: 1,
+              }}
+            >
+              <Ionicons name="trash" size={35} color="white" />
+            </View>
+          </TouchableHighlight>
+        );
+      };
     
   return (
-    <TouchableOpacity style = {styles.container} onPress={handleClick}>
-        <View style = {[styles.itemLeft, {width : width}]}>
-                    <View style={styles.imageContentContainer}>
-                    <View>
-                        <Text style={styles.imageTitle}>{title}</Text>
-                        <Text style={styles.imageSubtitle}>{"Joined: " + numJoined + "/" + size}</Text>
-                    </View>
-
+    <Swipeable
+    renderRightActions={renderRightActions}
+    overshootRight={false}
+    friction={2}
+    rightThreshold={40}
+    >
+        <TouchableHighlight style = {styles.container} onPress={handleClick}>
+            <View style = {[styles.itemLeft, {width : width}]}>
+                        <View style={styles.imageContentContainer}>
                         <View>
-                            <Image
-                            source={placeholder}
-                            style={[
-                                styles.avatarStyle,
-                                { zIndex: 1, marginLeft: (-125) },
-                            ]}
-                            />          
+                            <Text style={styles.imageTitle}>{title}</Text>
+                            <Text style={styles.imageSubtitle}>{"Joined: " + numJoined + "/" + size}</Text>
                         </View>
-                    </View>
-        </View>
-    </TouchableOpacity>
 
+                            <View>
+                                <Image
+                                source={placeholder}
+                                style={[
+                                    styles.avatarStyle,
+                                    { zIndex: 1, marginLeft: (-150) },
+                                ]}
+                                />          
+                            </View>
+                        </View>
+            </View>
+        </TouchableHighlight>
+    </Swipeable>
   );
 };
 
