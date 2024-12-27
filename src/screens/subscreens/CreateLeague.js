ed6975e845c635255f5425f707ac451b0c1773ea
Sid Dayaneni <79270579+sdayaneni@users.noreply.github.com>
2024-12-27 17:59:27 -0600

    Restructuring part 1

---
+++ src/screens/subscreens/CreateLeague.js
@@ -0,0 +1,237 @@
+import React from 'react';
+import LeagueCard from '../../components/LeagueCard';
+import { StyleSheet, Text, View, Image, TouchableOpacity, ScrollView, TextInput, Picker } from "react-native";
+import SelectDropdown from 'react-native-select-dropdown';
+import {getFirestore, collection, addDoc, doc, setDoc, getDoc, getDocs, deleteDoc, query, orderBy, getCountFromServer} from 'firebase/firestore';
+import {useState, useEffect} from 'react';
+import {db} from "../../../firebase.js"
+import { auth } from "../../../firebase";
+import { useNavigation } from '@react-navigation/core'
+import { Ionicons } from "@expo/vector-icons";
+import DateTimePicker from '@react-native-community/datetimepicker';
+import RNPickerSelect from 'react-native-picker-select';
+
+let leagueSize;
+let leagueType;
+export default function CreateLeague () {
+    const user = auth.currentUser;
+    const navigation = useNavigation();
+
+    const [leagueName, setLeagueName] = useState('');
+
+
+    async function handleCreateLeague(){
+        console.log(user.uid);
+        let toAddTo = collection(db, "leagues");
+        
+        const newLeague = await addDoc(toAddTo, {
+            name : leagueName,
+            size: leagueSize,
+            type: leagueType,
+            leagueManager: user.uid,
+            timeOfScheduling : 0,
+            timeOfDraft : 0,
+            draftStarted: false,
+            draftScheduled: false,
+            draftEnded: false,
+            turn: 0,
+            drafted: [],
+            round: 1
+          });
+
+          console.log(newLeague.id);
+          toAddTo = collection(db, "leagues", "" + newLeague.id, "members");
+
+          const snapshot = await getCountFromServer(toAddTo);
+          let numCurrent = snapshot.data().count;
+
+        await addDoc(toAddTo, {
+            memberId: user.uid,
+            wins: 0,
+            qb: "",
+            rb: "",
+            wr1: "",
+            wr2: "",
+            te: "",
+            bench1: "",
+            bench2: "",
+            bench3: "",
+            userTurn : numCurrent
+        });
+
+        navigation.navigate("Your Leagues");
+    }
+
+    return(
+        <View style = {styles.container}>
+            <View style = {{flexDirection: "row", marginBottom: 20, paddingHorizontal: 10}}>
+                <Text style = {styles.header}>Create League</Text>
+                <TouchableOpacity
+                    onPress={() => navigation.navigate("Settings")}
+                    style={{
+                        marginVertical: 'auto',
+                        marginLeft: 'auto',
+                    }}
+                    >
+                    <Ionicons
+                        name={"ellipsis-vertical"}
+                        size={25}
+                        color={"white"}
+                    />
+                </TouchableOpacity>
+            </View>
+
+            <ScrollView style = {{paddingHorizontal: 30,}}>
+
+                <View style={styles.inputTitle}>
+                    <Text style={styles.inputTitle}>League Name</Text>
+                    <TextInput
+                        onChangeText = {text => setLeagueName(text)}
+                        style={styles.input}
+                    />
+                </View>
+
+                <Text style={styles.inputTitle2}>League Size</Text>
+                <SelectDropdown
+                    data={[2, 4, 6, 8, 10, 12]}
+                    onSelect={(selectedItem, index) => {
+                        leagueSize = selectedItem
+                    }}
+                    buttonStyle={styles.dropdown1BtnStyle}
+                    defaultButtonText={"Select Size"}
+                    buttonTextStyle={styles.dropdown1BtnTxtStyle}
+
+                />
+
+                {/* <View
+                style={{
+                    display: "flex",
+                    flexDirection: "row",
+                    marginTop: 15
+                }}>
+                <Text style={{ marginVertical: "auto" }} fontWeight="bold">Game Mode</Text>
+                <View style={{ marginLeft: "auto" }}>
+                    <RNPickerSelect
+                        placeholder={{ label: 'Select a game mode...', value: null }}
+                        items={[
+                            { label: 'Classic', value: "Classic" },
+                            { label: 'Bingo', value: "Bingo" },
+                        ]}
+                        onValueChange={(value) => setEventType(value)}
+                        style={pickerStyles(true)}
+                        // disabled = {false}
+                    />
+                </View>
+                </View> */}
+
+                <Text style={styles.inputTitle2}>League Type</Text>
+                <SelectDropdown
+                    data={["Public", "Private"]}
+                    onSelect={(selectedItem, index) => {
+                        leagueType = selectedItem
+                    }}
+                    buttonStyle={styles.dropdown1BtnStyle}
+                    defaultButtonText={"Select Type"}
+                    buttonTextStyle={styles.dropdown1BtnTxtStyle}
+
+                />
+
+                <TouchableOpacity style={styles.submitContainer} onPress = {handleCreateLeague}>
+                    <Text style={[styles.text, {color: "#FFF", fontWeight: "bold", fontSize: 16}]}>Create</Text>
+                </TouchableOpacity>
+            </ScrollView>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        backgroundColor: "black",
+        paddingTop: 50,
+        height: 1000,
+    },
+    header: {
+        fontSize: 28,
+        fontWeight: 'bold',
+        color: "white",
+        paddingLeft: 20
+    },
+    inputTitle: {
+        color: "white",
+        fontSize: 15,
+        marginTop: 5,
+        fontWeight: 'bold',
+    },
+    input: {
+        flex: 1,
+        paddingHorizontal: 20, 
+        paddingVertical: 10,
+        marginTop: 10, 
+        borderColor: "#adacac", 
+        borderWidth: 2, 
+        borderRadius: 8, 
+        color: "#fff",
+        fontSize: 14,
+    },
+    inputTitle2: {
+        color: "white",
+        fontSize: 15,
+        marginTop: 75,
+        marginBottom: 25,
+        fontWeight: 'bold',
+    },
+    submitContainer: {
+        backgroundColor: "#9f86fc",
+        fontSize: 16,
+        borderRadius: 4,
+        paddingVertical: 12,
+        marginTop: 32,
+        alignItems: "center",
+        justifyContent: "center",
+        color: "#FFF",
+        // shadowColor: "#9f86fc",
+        // shadowOffset: { width: 0, height: 9 },
+        // shadowOpacity: 1,
+        // shadowRadius: 20,
+        elevation: 5,
+        marginTop: 100,
+        // fontWeight: 'bold',
+    },
+    dropdown1BtnStyle: {
+        flex: 1,
+        height: 50,
+        backgroundColor: "#9f86fc",
+        borderRadius: 8,
+        borderWidth: 1,
+        borderColor: "#444",
+        textColor: "white"
+      },
+      dropdown1BtnTxtStyle: { color: "white"},
+
+});
+
+const pickerStyles = (isDarkmode) => ({
+    inputIOS: {
+      fontSize: 14,
+      paddingVertical: 10,
+      paddingHorizontal: 10,
+      borderWidth: 1,
+      borderRadius: 8,
+      fontFamily: "Ubuntu_400Regular",
+      backgroundColor: isDarkmode ? "#1f1f1f" : themeColor.white,
+      borderColor: isDarkmode ? "#333333" : "#d8d8d8",
+      color: isDarkmode ? "#dddddd" : "black",
+    },
+    inputAndroid: {
+      fontSize: 14,
+      paddingHorizontal: 10,
+      paddingVertical: 8,
+      borderWidth: 1,
+      borderRadius: 8,
+      fontFamily: "Ubuntu_400Regular",
+      backgroundColor: isDarkmode ? "#1f1f1f" : themeColor.white,
+      borderColor: isDarkmode ? "#333333" : "#d8d8d8",
+      color: isDarkmode ? "#dddddd" : "black",
+    }
+  });
+  
---
+++ src/screens/subscreens/Roster.js
@@ -0,0 +1,433 @@
+import { StyleSheet, Text, View, Image, TouchableOpacity, ScrollView, TextInput, Picker, TouchableHighlight } from "react-native";
+import {getFirestore, collection, addDoc, doc, setDoc, getDoc, getDocs, deleteDoc, query, orderBy, updateDoc} from 'firebase/firestore';
+import {auth, db} from '../../../firebase.js';
+import React,{useState, useEffect, useRef} from 'react';
+import { useNavigation } from '@react-navigation/core';
+import { useIsFocused } from "@react-navigation/native";
+
+const fetch = require("node-fetch");
+// const cheerio = require("cheerio");
+
+const cfb = require('cfb.js');
+const defaultClient = cfb.ApiClient.instance;
+
+let user;
+let names = ["", "", "", "", "", "", "", ""];
+let startIndex = 0;
+let endIndex = 0;
+
+const URL = "https://www.espn.com/college-football/teams";
+
+let scraped2 = false;
+
+let memberDoc = null;
+let userDocId = "";
+
+
+export default function Roster({league, scraped}){
+    const isFocused = useIsFocused(); 
+    const navigation = useNavigation();
+
+    const [qb, setQb] = useState("");
+    const [rb, setRb] = useState("");
+    const [wr1, setWr1] = useState("");
+    const [wr2, setWr2] = useState("");
+    const [te, setTe] = useState("");
+    const [b1, setB1] = useState(["", ""]);
+    const [b2, setB2] = useState(["", ""]);
+    const [b3, setB3] = useState(["", ""]);
+
+    const [, updateState] = useState();
+    const forceUpdate = React.useCallback(() => updateState({}, []));
+
+    const [playerImages, setPlayerImages] = useState(['https://a.espncdn.com/combiner/i?img=/i/headshots/nophoto.png&w=200&h=146', 'https://a.espncdn.com/combiner/i?img=/i/headshots/nophoto.png&w=200&h=146', 'https://a.espncdn.com/combiner/i?img=/i/headshots/nophoto.png&w=200&h=146', 'https://a.espncdn.com/combiner/i?img=/i/headshots/nophoto.png&w=200&h=146', 'https://a.espncdn.com/combiner/i?img=/i/headshots/nophoto.png&w=200&h=146', 'https://a.espncdn.com/combiner/i?img=/i/headshots/nophoto.png&w=200&h=146', 'https://a.espncdn.com/combiner/i?img=/i/headshots/nophoto.png&w=200&h=146', 'https://a.espncdn.com/combiner/i?img=/i/headshots/nophoto.png&w=200&h=146']);
+
+    const [disabled,setDisabled]=useState([false, false, false, false, false, false, false, false]);
+    const [opacities,setOpacities]=useState([1, 1, 1, 1, 1, 1, 1,]);
+
+    const [textToDisplay, setTextToDisplay] = useState("Move");
+
+    // console.log('pulling data')
+    if(scraped == false) {
+      scraped2 = scraped;
+    }
+
+    useEffect(() => {
+
+        // console.log(scraped2);
+        if(isFocused){ 
+            user = auth.currentUser;
+            startFind();
+        }
+    }, [isFocused])
+
+
+    const getPlayerImages = async (name, index) => {
+      let currentPlayers = playerImages;
+      currentPlayers[index] = 'https://a.espncdn.com/combiner/i?img=/i/headshots/nophoto.png&w=200&h=146';
+      let toAddTo = collection(db, "leagues", "" + league, "drafted");
+      const q = query(toAddTo);
+      const querySnapshot = await getDocs(q);
+      let arrOfPlayers = [];
+      querySnapshot.forEach((doc) => {
+        if(name == doc.data().name) { 
+          currentPlayers[index] = doc.data().imageURL;
+        }
+      });
+      setPlayerImages(currentPlayers);
+      // console.log(doc.data().imageURL)
+      forceUpdate();
+    };
+
+    async function startFind() {
+        startIndex = 0;
+        endIndex = 0;
+        setDisabled([false, false, false, false, false, false, false, false]);
+        setOpacities([1, 1, 1, 1, 1, 1, 1,]);
+        setTextToDisplay("Move");
+
+        const q2 = query(collection(db, "leagues", "" + league, "members"));
+        const querySnapshot2 = await getDocs(q2);
+    
+        querySnapshot2.forEach((doc2) => {
+            if(doc2.data().memberId == user.uid) {
+              userDocId = doc2.id
+              memberDoc = doc2;
+            }
+        })
+      getRoster(memberDoc);
+    }
+
+    async function getRoster(doc2) {
+        setQb(doc2.data().qb);
+        names[0] = doc2.data().qb;
+        setRb(doc2.data().rb);
+        names[1] = doc2.data().rb;
+        setWr1(doc2.data().wr1);
+        names[2] = doc2.data().wr1;
+        setWr2(doc2.data().wr2);
+        names[3] = doc2.data().wr2;
+        setTe(doc2.data().te);
+        names[4] = doc2.data().te;
+        setB1([doc2.data().bench1[0], doc2.data().bench1[1]]);
+        names[5] = doc2.data().bench1[0];
+        setB2([doc2.data().bench2[0], doc2.data().bench2[1]]);
+        names[6] = doc2.data().bench2[0];
+        setB3([doc2.data().bench3[0], doc2.data().bench3[1]]);
+        names[7] = doc2.data().bench3[0];
+
+        // if(playerImages[0] == 'https://a.espncdn.com/combiner/i?img=/i/headshots/nophoto.png&w=200&h=146') {
+        getPlayerImages(names[0], 0);
+        getPlayerImages(names[1], 1);
+        getPlayerImages(names[2], 2);
+        getPlayerImages(names[3], 3);
+        getPlayerImages(names[4], 4);
+        getPlayerImages(names[5], 5);
+        getPlayerImages(names[6], 6);
+        getPlayerImages(names[7], 7);
+          // scraped2 = true;
+        // }
+    }
+
+    function refresh() {
+      startIndex = 0;
+      endIndex = 0;
+      setDisabled([false, false, false, false, false, false, false, false]);
+      setOpacities([1, 1, 1, 1, 1, 1, 1,]);
+      setTextToDisplay("Move");
+
+      setQb(names[0]);
+      setRb(names[1]);
+      setWr1(names[2]);
+      setWr2(names[3]);
+      setTe(names[4]);
+      setB1([names[5], b1[1]]);
+      setB2([names[6], b2[1]]);
+      setB3([names[7], b3[1]]);
+
+
+      getPlayerImages(names[0], 0);
+      getPlayerImages(names[1], 1);
+      getPlayerImages(names[2], 2);
+      getPlayerImages(names[3], 3);
+      getPlayerImages(names[4], 4);
+      getPlayerImages(names[5], 5);
+      getPlayerImages(names[6], 6);
+      getPlayerImages(names[7], 7);
+    }
+
+ 
+    //fix up bench
+    function selectPlayer(position, name, index) {
+      if(textToDisplay == "Move") {
+        if(position == "QB") {
+          setDisabled([false, true, true, true, true, (b1[1] != position), (b2[1] != position), (b3[1] != position)]);
+          setOpacities([1, .3, .3, .3, .3, b1[1] == position ? 1 : .3, b2[1] == position ? 1 : .3, b3[1] == position ? 1 : .3])
+        }
+        else if(position == "RB") {
+          setDisabled([true, false, true, true, true, (b1[1] != position), (b2[1] != position), (b3[1] != position)]);
+          setOpacities([.3, 1, .3, .3, .3, b1[1] == position ? 1 : .3, b2[1] == position ? 1 : .3, b3[1] == position ? 1 : .3]);
+        }
+        else if(position == "WR") {
+          setDisabled([true, true, false, false, true, (b1[1] != position), (b2[1] != position), (b3[1] != position)]);
+          setOpacities([.3, .3, 1, 1, .3, b1[1] == position ? 1 : .3, b2[1] == position ? 1 : .3, b3[1] == position ? 1 : .3])
+        }
+        else if(position == "TE") {
+          setDisabled([true, true, true, true, false, (b1[1] != position), (b2[1] != position), (b3[1] != position)]);
+          setOpacities([.3, .3, .3, .3, 1, b1[1] == position ? 1 : .3, b2[1] == position ? 1 : .3, b3[1] == position ? 1 : .3])
+        }
+        else if(position == "B") {
+          getBenchPosition(name, index);
+        }
+        startIndex = index;
+        setTextToDisplay("Here");
+      }
+
+      else if(textToDisplay == "Here") {
+        rearrangePlayers(position, name, index);
+      }
+    }
+
+    async function rearrangePlayers(position, name, index) {
+      let endIndex = index;
+      temp = names[startIndex];
+      names[startIndex] = names[endIndex];
+      names[endIndex] = temp;
+
+      const q2 = query(collection(db, "leagues", "" + league, "members"));
+      const querySnapshot2 = await getDocs(q2);
+
+      if(userDocId == "") {
+        querySnapshot2.forEach((doc2) => {
+            if(doc2.data().memberId == user.uid) {
+              userDocId = doc2.id;
+            }
+        })
+      }
+
+      let toUpdate = doc(db, "leagues", "" + league, "members", "" + userDocId);
+      await updateDoc(toUpdate, {
+        qb: names[0],
+        rb: names[1],
+        wr1: names[2],
+        wr2: names[3],
+        te: names[4],
+        bench1: names[5] == "" ? "" : [names[5], b1[1]],
+        bench2: names[6] == "" ? "" : [names[6], b2[1]],
+        bench3: names[7] == "" ? "" : [names[7], b3[1]]
+      });
+
+      refresh();
+
+      // startFind();
+    }
+
+    async function getBenchPosition(name, index) {
+      // const ApiKeyAuth = defaultClient.authentications['ApiKeyAuth'];
+      // ApiKeyAuth.apiKey = "Bearer iEgwLD0Q2/fKfE2JrnvQXS2kb4Yim4jMK4PxkPQGiaQ7EJX54+W8ZrTwuVgB6vJ6";
+  
+      // var api = new cfb.PlayersApi();
+      // var player = await api.playerSearch(name);
+      if(index == 5) {
+        selectPlayer(b1[1], name, index);
+      }
+      if(index == 6) {
+        selectPlayer(b2[1], name, index);
+      }
+      if(index == 7) {
+        selectPlayer(b3[1], name, index);
+      }
+      
+    }
+
+    const PlayerCell = ({position, name, navigation, index}) => {
+      return  (
+          <TouchableOpacity disabled = {disabled[index]} style={[styles.item, {opacity: opacities[index]}]} onPress={() => showPlayerInfo(name, navigation)}>
+              <View style={[styles.itemLeft]}>
+                  <View style = {{}}>
+                    <View style={[styles.square]}>
+                        <Text style = {styles.positionText}>{position}</Text>
+                    </View>
+                  </View>
+                  <View style = {{}}>
+                    <Image source={{uri: playerImages[index]}} style = {{width: 50, height: 50, marginLeft: -5}}></Image> 
+                  </View>
+                  <Text style={styles.itemText}>{name}</Text>
+              </View>
+              <TouchableOpacity  disabled = {disabled[index]} style = {{backgroundColor: '#9f86fc', opacity: 0.9, borderRadius: 5, justifyContent: "center", width: 75, height: 30, marginBottom: 10}} onPress={() => selectPlayer(position, name, index)}>
+                <View>
+                    <Text style = {[styles.sectionTitle, {fontSize: 16, textAlign: "center", marginBottom: 0}]}>{textToDisplay}</Text>
+                </View>
+              </TouchableOpacity>
+          </TouchableOpacity>
+      )
+    }
+  
+    function showPlayerInfo(name, navigation){
+      // console.log('showing player info on: ' + name);
+      navigation.navigate("PlayerInfo", {
+        playerName: name,
+        league: league
+    });
+    }
+
+    async function dropPlayer () {
+      let toUpdate2 = doc(db, "leagues", "" + league);
+      let document = await getDoc(toUpdate2);
+      currentDrafted = document.data().drafted;
+
+      indexToFind = currentDrafted.indexOf(names[startIndex]);
+
+      currentDrafted.splice(indexToFind, 1);
+
+     await updateDoc(toUpdate2, {
+        drafted: currentDrafted
+      });
+
+      let name = names[startIndex];
+
+      names[startIndex] = "";
+
+      const q2 = query(collection(db, "leagues", "" + league, "members"));
+      const querySnapshot2 = await getDocs(q2);
+
+      if(userDocId == "") {
+        querySnapshot2.forEach((doc2) => {
+            if(doc2.data().memberId == user.uid) {
+              userDocId = doc2.id;
+            }
+        })
+      }
+
+      let toUpdate = doc(db, "leagues", "" + league, "members", "" + userDocId);
+
+      await updateDoc(toUpdate, {
+        qb: names[0],
+        rb: names[1],
+        wr1: names[2],
+        wr2: names[3],
+        te: names[4],
+        bench1: names[5] == "" ? "" : [names[5], b1[1]],
+        bench2: names[6] == "" ? "" : [names[6], b2[1]],
+        bench3: names[7] == "" ? "" : [names[7], b3[1]]
+      });
+
+      let toAddTo = collection(db, "leagues", "" + league, "drafted");
+      const q = query(toAddTo);
+      const querySnapshot = await getDocs(q);
+      let arrOfPlayers = [];
+      querySnapshot.forEach((doc) => {
+        if(name == doc.data().name) {
+          // console.log(doc.data().name);
+          // console.log(name);
+          del(doc.id);
+        }
+      });
+
+      let currentPlayers = playerImages;
+      currentPlayers[startIndex] = 'https://a.espncdn.com/combiner/i?img=/i/headshots/nophoto.png&w=200&h=146';
+      setPlayerImages(currentPlayers);
+      forceUpdate();
+
+
+      // startFind();
+      refresh();
+    }
+
+    async function del(document){
+      await deleteDoc(doc(db, "leagues", "" + league, "drafted", document));
+    }
+
+
+    return (
+      <View style = {styles.container}>
+      <View style = {{flexDirection: "row", width: 600,}}>
+        <Text style={styles.sectionTitle}>Your Squad</Text>
+        {(textToDisplay == "Here")?
+              <TouchableOpacity style = {{backgroundColor: 'red', opacity: 0.9, borderRadius: 5, justifyContent: "center", width: 75, height: 30, marginLeft: 100, marginTop: 5}} onPress={() => dropPlayer()}>
+                  <View>
+                      <Text style = {[styles.sectionTitle, { fontSize: 16, textAlign: "center", marginBottom: 0}]}>Drop</Text>
+                  </View>
+              </TouchableOpacity>
+          : null }
+        </View>
+          <ScrollView>
+
+            <View style = {{height: 750}}>
+              <PlayerCell position = "QB" name = {qb} navigation = {navigation} index = {0}></PlayerCell>
+              <PlayerCell position = "RB" name = {rb} navigation = {navigation} index = {1}></PlayerCell>
+              <PlayerCell position = "WR" name = {wr1} navigation = {navigation} index = {2}></PlayerCell>
+              <PlayerCell position = "WR" name = {wr2} navigation = {navigation} index = {3}></PlayerCell>
+              <PlayerCell position = "TE" name = {te} navigation = {navigation} index = {4}></PlayerCell>
+              <PlayerCell position = "B" name = {b1[0]} navigation = {navigation} index = {5}></PlayerCell>
+              <PlayerCell position = "B" name = {b2[0]} navigation = {navigation} index = {6}></PlayerCell>
+              <PlayerCell position = "B" name = {b3[0]} navigation = {navigation} index = {7}></PlayerCell>
+            </View>
+
+      </ScrollView>
+      </View>
+    );
+}
+
+
+const styles = StyleSheet.create({
+    container: {
+        paddingTop: 60,
+        padding: 20,
+        backgroundColor: 'black',
+        width: 400,
+        // height: 1500
+    },
+    item: {
+        backgroundColor: '#211f1f',
+        padding: 10,
+        borderRadius: 10,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        marginBottom: 18,
+        paddingBottom: 0
+    },
+    itemLeft: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      flexWrap: 'wrap',
+      height: '100%',
+    },
+    square: {
+      width: 33,
+      height: 33,
+      backgroundColor: '#9f86fc',
+      opacity: 0.9,
+      borderRadius: 5,
+      marginRight: 15,
+      justifyContent: "center",
+      marginBottom: 10
+    },
+    itemText: {
+      maxWidth: '61%',
+      color: "white",
+      fontWeight: "500",
+      fontSize: 14,
+      marginLeft: 8,
+      paddingBottom: 10
+    },
+    circular: {
+      width: 12,
+      height: 12,
+      borderColor: '#9f86fc',
+      borderWidth: 2,
+      borderRadius: 5,
+    },
+    sectionTitle: {
+        fontSize: 24,
+        fontWeight: 'bold',
+        marginBottom: 20,
+        color: "white"
+    },
+    positionText: {
+        textAlign: "center",
+        color: "white",
+        // fontWeight: 'bold'
+    }
+  });
