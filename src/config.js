10e0e9be209e82220813ffcd3c41daa156728ad9
Sid Dayaneni <79270579+sdayaneni@users.noreply.github.com>
2025-01-12 01:11:43 -0600

    Added config for API Gateway

---
+++ src/config.js
@@ -0,0 +1,7 @@
+export default {
+    API_GATEWAY_URL: 'https://your-api-id.execute-api.region.amazonaws.com/dev',
+  };
+  
+//USING API GATEWAY IN PROJECT-------------------------
+// import config from './config';
+// axios.get(`${config.API_GATEWAY_URL}/logos`);
